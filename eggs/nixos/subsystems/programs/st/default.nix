0681cbedd727d1d4c22dba591b16c1c31e8b603c
KiraStack <dennischris56@gmail.com>
2026-03-15 16:12:11 +0000

    chore: boost picom

--- eggs/nixos/subsystems/programs/st/default.nix
+++ eggs/nixos/subsystems/programs/st/default.nix
@@ -10,6 +10,10 @@
           url = "https://st.suckless.org/patches/ligatures/0.8.3/st-ligatures-20200430-0.8.3.diff";
           sha256 = "vKiYU0Va/iSLhhT9IoUHGd62xRD/XtDDjK+08rSm1KE=";
         })
+        (pkgs.fetchpatch {
+          url = "https://st.suckless.org/patches/clipboard/st-clipboard-0.8.3.diff";
+          sha256 = "1h1nwilwws02h2lnxzmrzr69lyh6pwsym21hvalp9kmbacwy6p0g";
+        })
       ];
       # Using a local file
       configFile = pkgs.writeText "config.def.h" (builtins.readFile ./config.h);
